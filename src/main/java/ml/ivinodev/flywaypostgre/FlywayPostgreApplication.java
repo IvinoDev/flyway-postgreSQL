5e40779a1324903f569d1cd50a097c2a8a85d2e9
IvinoDev <kayantao@protonmail.com>
2023-05-13 10:28:37 +0000

    initial commit

---
+++ src/main/java/ml/ivinodev/flywaypostgre/FlywayPostgreApplication.java
@@ -0,0 +1,13 @@
+package ml.ivinodev.flywaypostgre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlywayPostgreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlywayPostgreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ml/ivinodev/flywaypostgre/FlywayPostgreApplicationTests.java
@@ -0,0 +1,13 @@
+package ml.ivinodev.flywaypostgre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlywayPostgreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
